c056cb17f0da0d983346a4cf7662270cd9ba4490
Jenner LaFave <jenner@jfave.com>
2012-07-07 09:34:10 -0500

    added ssh config to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,10 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "ssh_config "-av . ~
+}
+function cp_ssh_cfg() {
+  cp ssh_config ~/.ssh/config && chmod 0600 ~/.ssh/config
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
@@ -10,8 +13,12 @@ else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
+		echo "Copying dotfiles..."
+    doIt
+    echo "Copying ssh config..."
+    ([[-d ~/.ssh]] && cp_ssh_cfg) || mkdir ~/.ssh && cp_ssh_cfg
 	fi
 fi
 unset doIt
+unset cp_ssh_cfg
 source ~/.bash_profile
