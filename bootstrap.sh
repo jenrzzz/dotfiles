ad0e7b9608da6318c00472b83e10211fecac4156
Jenner LaFave <jenner@jfave.com>
2012-07-16 17:52:10 -0700

    Added vundle to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,19 @@ function doIt() {
 function cp_ssh_cfg() {
   cp ssh_config ~/.ssh/config && chmod 0600 ~/.ssh/config
 }
+function install_vundle() {
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+}
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
+    if [[ ! -d ~/.vim/bundle/vundle ]]; then
+      echo "Installing vundle..."
+      install_vundle
+    fi
 		echo "Copying dotfiles..."
     doIt
     echo "Copying ssh config..."
